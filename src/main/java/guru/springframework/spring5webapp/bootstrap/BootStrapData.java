687cfff169cb649bbd65458884c7588fed9c58dd
arthur <arthurcontroleambiental@gmail.com>
2022-10-04 16:55:29 -0300

    adding a Publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,15 @@ de dependencias lembrar que ela é um esteriótipo para as
 @Repository, @Service e @Controller*/
 
 public class BootStrapData implements CommandLineRunner {
-    AuthorRepository authorRepository;
-    BookRepository bookRepository;
+    final AuthorRepository authorRepository;
+    final BookRepository bookRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,9 +45,16 @@ public class BootStrapData implements CommandLineRunner {
 
         authorRepository.save(stewart);
         bookRepository.save(book1);
+
+        Publisher amazon = new Publisher("Amazon","QR 327 conjunto 2","Paraíba",
+                "Joao Pessoa", "3213213" );
+
+        publisherRepository.save(amazon);
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of Authors: " + authorRepository.count());
+        System.out.println("Number of Publishers "+ publisherRepository.count());
+
     }
 
 }
